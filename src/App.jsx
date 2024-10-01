ca094ab948be72449433bc2b2f6f86b92d90b917
Janelle <2264502@cegeplimoilou.ca>
2024-10-01 16:59:36 -0400

    ✨Added boostrap-icon library

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Title from "./components/Title.jsx";
 import Navbar from "./components/Navbar.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
 

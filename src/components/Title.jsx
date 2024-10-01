18b1a75d1329af64c92ee0102f665caebac25c83
Janelle <2264502@cegeplimoilou.ca>
2024-10-01 17:27:44 -0400

    ✨Modified icons appearance

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,25 @@
 export default function Title() {
 
-    return(
+    return (
 
         <>
-            <h1 className="main-title"> Welcome on Janelle's Portfolio</h1>
-        </>
-    )
+            <div className="container">
+                <div className="row">
+                    <div className="col d-flex justify-content-center">
+                        <h1 className="main-title">aaaa</h1>
+                    </div>
+                </div>
+                <hr/>
+
+                <div className="row">
+                    <div className="col-6 d-flex justify-content-center">
+                        <a href="https://www.linkedin.com/in/janellebed/" className="icon-socials"> <i className="bi bi-linkedin pe-5 fs-4"/> </a>
+                        <a href="https://github.com/jajanel" className=" icon-socials"><i className="bi bi-github pe-5 fs-4 icon-socials"/></a>
+                        <a href="mailto:janellebedard@gmail.com?subject=Contact%20from%20portfolio"
+                           className="icon-socials"><i className="bi bi-envelope-fill pe-5 fs-4"/> </a>
+                    </div>
+
+                </div>
+            </div>
+        </>)
 }

0197fc1d6c0538216c19ba760311d7edc6a1750b
Janelle <2264502@cegeplimoilou.ca>
2024-10-01 15:49:28 -0400

    ✨Added background and title

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Title from "./components/Title.jsx";
+
+function App() {
+
+    return (
+        <>
+            <Title/>
+        </>
+    )
+}
+
+export default App
---
+++ src/components/Title.jsx
@@ -0,0 +1,9 @@
+export default function Title() {
+
+    return(
+
+        <>
+            <h1 className="main-title"> Welcome on Janelle's Portfolio</h1>
+        </>
+    )
+}

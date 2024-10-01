cda7d2c032f3a79a8af85fac1e8bb9be4568e011
Janelle <2264502@cegeplimoilou.ca>
2024-10-01 16:43:10 -0400

    ✨Imported boostrap for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import Title from "./components/Title.jsx";
+import Navbar from "./components/Navbar.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function App() {
 
     return (
         <>
+            <Navbar/>
             <Title/>
         </>
     )

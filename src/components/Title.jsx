eea0e642bacd60282640e24ae93aea7b913e7ecb
JanelleB2 <2264502@cegeplimoilou.ca>
2024-10-02 00:21:16 -0400

    ✨ Modified and added bootstrap and CSS styling

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,23 +3,25 @@ export default function Title() {
     return (
 
         <>
-            <div className="container">
+            <div className="container title-container">
                 <div className="row">
-                    <div className="col d-flex justify-content-center">
-                        <h1 className="main-title">aaaa</h1>
+                    <div className="col ">
+                        <h1 className="main-title">hi, i&#39;m janelle</h1>
+                        <h2 className="sub-title"> a third year student in app development</h2>
                     </div>
                 </div>
                 <hr/>
 
                 <div className="row">
-                    <div className="col-6 d-flex justify-content-center">
-                        <a href="https://www.linkedin.com/in/janellebed/" className="icon-socials"> <i className="bi bi-linkedin pe-5 fs-4"/> </a>
-                        <a href="https://github.com/jajanel" className=" icon-socials"><i className="bi bi-github pe-5 fs-4 icon-socials"/></a>
+                    <div className="col d-flex justify-content-center">
+                        <a href="https://www.linkedin.com/in/janellebed/" className="icon-socials"> <i className="bi bi-linkedin mx-4 fs-4"/> </a>
+                        <a href="https://github.com/jajanel" className=" icon-socials"><i className="bi bi-github mx-4 fs-4 icon-socials"/></a>
                         <a href="mailto:janellebedard@gmail.com?subject=Contact%20from%20portfolio"
-                           className="icon-socials"><i className="bi bi-envelope-fill pe-5 fs-4"/> </a>
+                           className="icon-socials"><i className="bi bi-envelope-fill mx-4 fs-4"/> </a>
                     </div>
 
                 </div>
             </div>
-        </>)
+        </>
+    )
 }

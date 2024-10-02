64da393fdc35fbad7ea28213c783900460114cee
JanelleB2 <2264502@cegeplimoilou.ca>
2024-10-02 00:20:01 -0400

    ✨ Added an array of projects and imported it

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import './App.css'
+import './App.css';
 import Title from "./components/Title.jsx";
 import Navbar from "./components/Navbar.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import Card from "./components/Card.jsx";
+import { dataProjects } from "./assets/projectInformation.js";
 
 function App() {
-
     return (
         <>
-            <Navbar/>
-            <Title/>
+            <Navbar />
+            <Title />
+            <Card dataProjects={dataProjects} />
         </>
-    )
+    );
 }
 
-export default App
+export default App;
---
+++ src/assets/projectInformation.js
@@ -0,0 +1,15 @@
+export const dataProjects = [
+    {
+        idProject: 1,
+        projectTitle: "Feeding the animals",
+        projectDescription: "This space-invader type of game is about feeding the animals before they reach the bottom of the screen",
+        datePublication: "septembre 2024",
+        projectImage: "https://play.unity.com/_next/image?url=https%3A%2F%2Fplay.unity.com%2Fapi%2Fv1%2Ffiles%2Ffile%2Ff537ba37-87f2-4479-b1dd-55214e201b34%2Fcontent&w=3840&q=75"
+    },
+    {
+        idProject: 2,
+        projectTitle: "Bird appreciation platform",
+        projectDescription: "a web app to add, edit, delete and view birds and their reviews",
+        datePublication: "october 2024",
+        projectImage: "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg"
+    }];
